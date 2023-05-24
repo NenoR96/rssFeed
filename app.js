c06fd9931133884c00de6d30fa2cd112e2a1740c
Nedeljko Ružić <33520721+NenoR96@users.noreply.github.com>
2023-05-24 18:24:57 +0200

    random posts

--- app.js
+++ app.js
@@ -6,28 +6,31 @@ const app = express()
 const port = 3000
 const convert = require('xml-js');
 
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 app.get('/', (req, res) => {
     fs.readFile('xml.json', 'utf8', function (err, fileData) {
         if (err) throw err;
         let data = JSON.parse(fileData);
 
-        [1,2,3,4,5].forEach((element) => {
+        [...Array(randomIntFromInterval(1, 6))].forEach((element, index) => {
             let obj = {
-                title: "Random post " + element,
-                link: "blabla",
+                title: "Random post " + (index + 1),
+                link: "https://www.lipsum.com/",
                 guid: crypto.randomBytes(16).toString("hex"),
                 pubDate: new Date().toGMTString(),
-                description: "teasdasdasd",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
                 source: "hehe"
             }
-            data.rss.channel.item.push(obj);
+            data.rss.channel.item.unshift(obj);
         });
 
         const json = JSON.stringify(data);
         const xml = convert.json2xml(json, { compact: true, spaces: 4 });
         res.type("text/xml").send(xml);
     });
-
 })
 
 app.listen(port, () => {
